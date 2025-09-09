dfb46074d17a9bbb76bc55bbe3e622aaa7f164d0
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-09-09 18:46:15 +0200

    feat: snack bonus 6

--- snackBonus.js
+++ snackBonus.js
@@ -1,57 +1,58 @@
 // 🎯 Code Question 5 (Bonus)
 
-const hamburger = {
-  name: "Cheese Burger",
-  weight: 250,
-  maker: {
-    name: "Anonymous Chef",
-    restaurant: {
-      name: "Hyur's Burgers",
-      address: "Main Street, 123",
-      isOpen: true,
-    },
-    age: 29,
-  },
-};
+// const hamburger = {
+//   name: "Cheese Burger",
+//   weight: 250,
+//   maker: {
+//     name: "Anonymous Chef",
+//     restaurant: {
+//       name: "Hyur's Burgers",
+//       address: "Main Street, 123",
+//       isOpen: true,
+//     },
+//     age: 29,
+//   },
+// };
 
-const newRestaurant = { ...hamburger.maker.restaurant };
-newRestaurant.name = "Hyur's II";
-newRestaurant.address = "Second Street, 12";
-const secondBurger = { ...hamburger };
-secondBurger.maker.restaurant = newRestaurant;
-secondBurger.maker.name = "Chef Hyur";
-console.log(hamburger.maker.name); // Chef Hyur
-console.log(secondBurger.maker.name); // Chef Hyur
-console.log(hamburger.maker.restaurant.name); // Hyur's II
-console.log(secondBurger.maker.restaurant.name); // Hyur's II
+// const newRestaurant = { ...hamburger.maker.restaurant };
+// newRestaurant.name = "Hyur's II";
+// newRestaurant.address = "Second Street, 12";
+// const secondBurger = { ...hamburger };
+// secondBurger.maker.restaurant = newRestaurant;
+// secondBurger.maker.name = "Chef Hyur";
+// console.log(hamburger.maker.name); // Chef Hyur
+// console.log(secondBurger.maker.name); // Chef Hyur
+// console.log(hamburger.maker.restaurant.name); // Hyur's II
+// console.log(secondBurger.maker.restaurant.name); // Hyur's II
 
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 5
 
 // 🎯 Code Question 6 (Bonus)
 
-// const chef = {
-//     name: "Chef Hyur",
-//     age: 29,
-//     makeBurger: (num = 1) => {
-//         console.log(`Ecco ${num} hamburger per te!`);
-//     },
-//     restaurant: {
-//         name: "Hyur's Burgers",
-//         welcomeClient: () => {
-//             console.log("Benvenuto!");
-//         },
-//         address: {
-//             street: 'Main Street',
-//             number: 123,
-//             showAddress: () => {
-//                 console.log("Main Street 123");
-//             }
-//         },
-//         isOpen: true,
-//     }
-// }
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+    restaurant: {
+        name: "Hyur's Burgers",
+        welcomeClient: () => {
+            console.log("Benvenuto!");
+        },
+        address: {
+            street: 'Main Street',
+            number: 123,
+            showAddress: () => {
+                console.log("Main Street 123");
+            }
+        },
+        isOpen: true,
+    }
+}
 // Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+// Probabilmente lo spread operatore perchè sono contenute diverse funzioni, vanno fatti però più spread operator perchè ci sono più oggetti annidati
 
 // 🎯 Snack  (Bonus)
 
